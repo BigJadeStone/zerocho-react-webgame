60dcc54941b345bbf88a6fcf97466678c87beb17
taemin <tamin0112@gmail.com>
2025-05-27 19:28:03 +0900

    commit

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,6 +1,16 @@
+// const React = require('react');
+// const ReactDom = require('react-dom');
+//
+// const WordRelay = require('./WordRelay');
+//
+// ReactDom.render(<WordRelay />, document.querySelector('#root'));
+
+
 const React = require('react');
-const ReactDom = require('react-dom');
+const ReactDom = require('react-dom/client'); // 경로 변경
 
 const WordRelay = require('./WordRelay');
 
-ReactDom.render(<WordRelay />, document.querySelector('#root'));
+// React 18+ 방식: createRoot 사용
+const root = ReactDom.createRoot(document.querySelector('#root'));
+root.render(<WordRelay />);
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -2,48 +2,29 @@ const path = require('path');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-  name: 'wordrelay',
+  name: 'wordrelay-setting',
   mode: 'development', // 실서비스: production
   devtool: 'eval', // 실서비스: hidden-source-map
   resolve: {
     extensions: ['.js', '.jsx']
   },
-
   entry: {
-    app: ['./client'],
-  }, // 입력
+    app: ['./client'], //확장자 굳이 안 적어도됨. 나중에 json, css 등등 많이들어옴. -> resolve에 확장자만 추가하면 됨.
+  }, //입력
+
   module: {
     rules: [{
       test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: [
-          ['@babel/preset-env', {
-            targets: {
-              browsers: ['> 1% in KR'], // browserslist
-            },
-            debug: true,
-          }],
-          '@babel/preset-react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          'react-refresh/babel',
-        ],
-      },
-    }],
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    }]
   },
-  plugins: [
-    new RefreshWebpackPlugin()
-  ],
+
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/dist/',
-  },
-  devServer: {
-    devMiddleware: { publicPath: '/dist' },
-    static: { directory: path.resolve(__dirname) },
-    hot: true
-  },
+    path: path.join(__dirname, 'dist'), // C:\\users\zerocho]webstorm\react-webgame\lecture\dist
+    filename: 'app.js'
+  }, //출력
+
 };
