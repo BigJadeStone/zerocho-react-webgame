bc459216ba897ea1ac717291bfeb32510d7f6a65
JadeStone <tamin0112@gmail.com>
2025-06-07 00:50:04 +0900

    commit

--- lecture/MyTd.jsx
+++ lecture/MyTd.jsx
@@ -1,5 +1,5 @@
 import React, {memo, useCallback, useContext, useEffect, useMemo, useRef} from "react";
-import {CODE, TableContext} from "./MyMineSearch";
+import {CLICK_MINE, CODE, FLAG_CELL, NORMALIZE_CELL, OPEN_CELL, QUESTION_CELL, TableContext} from "./MyMineSearch";
 
 const getTdStyle = (code) => {
     switch (code) {
@@ -52,11 +52,61 @@ const getTdText = (code) => {
 
 const MyTd = memo(({ rowIndex, cellIndex }) => {
     const { tableData, dispatch, halted } = useContext(TableContext);
-    const data = tableData[rowIndex][cellIndex];
+    const data = tableData[rowIndex][cellIndex]; //cell의 CODE
 
+    const onClickTd = useCallback(() => {
+        if (halted) {
+            return;
+        }
 
+        switch (data) {
+            case CODE.OPENED:
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                return;
+            case CODE.NORMAL:
+                dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.MINE:
+                dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+
+    }, [data, halted]);
+
+    const onRightClickTd = useCallback((e) => {
+        e.preventDefault();
+        if (halted) {
+            return;
+        }
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+                dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+                dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    console.log('td rendered');
     return (
-        <td style={getTdStyle(data)}>
+        <td style={getTdStyle(data)}
+            onClick={onClickTd}
+            onContextMenu={onRightClickTd}
+        >
             {getTdText(data)}
         </td>
     )
